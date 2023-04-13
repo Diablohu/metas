e48d97fe46382e9f66bb7c1dbb012d6fb19ffbcb
Diablohu <huxin@cmcm.com>
2023-04-13 19:18:03 +0800

    re-format

--- src/metas.js
+++ src/metas.js
@@ -1,5 +1,6 @@
-module.exports = (options = {
-    /*
+module.exports = (
+    options = {
+        /*
     // page infos
         title
         description
@@ -9,10 +10,10 @@ module.exports = (options = {
         charset
         locale
         type
-    
+
     // site infos
         siteName
-    
+
     // for Twitter
         twitter.card
         twitter.siteCreator
@@ -22,170 +23,180 @@ module.exports = (options = {
         facebook.adminsId
         facebook.appId
     */
-}, returnArray = false) => {
-    let metas = []
-    let o = Object.assign({
-        charset: 'utf-8',
-        twitter: {},
-        facebook: {}
-    }, options)
+    },
+    returnArray = false
+) => {
+    const metas = [];
+    const o = Object.assign(
+        {
+            charset: 'utf-8',
+            twitter: {},
+            facebook: {},
+        },
+        options
+    );
 
     if (typeof window !== 'undefined') {
         if (typeof o.title === 'undefined' && document.title)
-            o.title = document.title
-        if (typeof o.url === 'undefined' && typeof location !== 'undefined' && location.href.substr(0, 5) !== 'about')
-            o.url = location.href
+            o.title = document.title;
+        if (
+            typeof o.url === 'undefined' &&
+            typeof window.location !== 'undefined' &&
+            window.location.href.substr(0, 5) !== 'about'
+        )
+            o.url = window.location.href;
     }
 
     if (typeof o.charset !== 'undefined') {
         metas.push({
-            charset: o.charset
-        })
+            charset: o.charset,
+        });
     }
 
     if (typeof o.title !== 'undefined') {
         metas.push({
             itemprop: 'name',
-            content: o.title
-        })
+            content: o.title,
+        });
         metas.push({
             name: 'twitter:title',
-            content: o.title
-        })
+            content: o.title,
+        });
         metas.push({
             property: 'og:title',
-            content: o.title
-        })
+            content: o.title,
+        });
     }
 
     if (typeof o.description !== 'undefined') {
         metas.push({
             name: 'description',
-            content: o.description
-        })
+            content: o.description,
+        });
         metas.push({
             itemprop: 'description',
-            content: o.description
-        })
+            content: o.description,
+        });
         metas.push({
             name: 'twitter:description',
-            content: o.description
-        })
+            content: o.description,
+        });
         metas.push({
             property: 'og:description',
-            content: o.description
-        })
+            content: o.description,
+        });
     }
 
     if (typeof o.image !== 'undefined') {
         metas.push({
             name: 'image',
-            content: o.image
-        })
+            content: o.image,
+        });
         metas.push({
             itemprop: 'image',
-            content: o.image
-        })
+            content: o.image,
+        });
         metas.push({
             name: 'twitter:image:src',
-            content: o.image
-        })
+            content: o.image,
+        });
         metas.push({
             property: 'og:image',
-            content: o.image
-        })
+            content: o.image,
+        });
     }
 
     if (typeof o.video !== 'undefined') {
         metas.push({
             name: 'twitter:player',
-            content: o.video
-        })
+            content: o.video,
+        });
         metas.push({
             property: 'og:video',
-            content: o.video
-        })
+            content: o.video,
+        });
     }
 
     if (typeof o.url !== 'undefined') {
         metas.push({
             property: 'og:url',
-            content: o.url
-        })
+            content: o.url,
+        });
     }
 
     if (typeof o.siteName !== 'undefined') {
         metas.push({
             property: 'og:site_name',
-            content: o.siteName
-        })
+            content: o.siteName,
+        });
     }
 
     if (typeof o.locale !== 'undefined') {
         metas.push({
             property: 'og:locale',
-            content: o.locale
-        })
+            content: o.locale,
+        });
     }
 
     if (typeof o.type !== 'undefined') {
         metas.push({
             property: 'og:type',
-            content: o.type
-        })
+            content: o.type,
+        });
     }
 
     if (typeof o.twitter.card !== 'undefined') {
         metas.push({
             name: 'twitter:card',
-            content: o.twitter.card
-        })
+            content: o.twitter.card,
+        });
     }
 
     if (typeof o.twitter.siteCreator !== 'undefined') {
         metas.push({
             name: 'twitter:site',
-            content: getTwitterUserName(o.twitter.siteCreator)
-        })
+            content: getTwitterUserName(o.twitter.siteCreator),
+        });
     }
 
     if (typeof o.twitter.author !== 'undefined') {
         metas.push({
             name: 'twitter:creator',
-            content: getTwitterUserName(o.twitter.author)
-        })
+            content: getTwitterUserName(o.twitter.author),
+        });
     }
 
     if (typeof o.facebook.adminsId !== 'undefined') {
         metas.push({
             name: 'fb:admins',
-            content: getTwitterUserName(o.facebook.adminsId)
-        })
+            content: getTwitterUserName(o.facebook.adminsId),
+        });
     }
 
     if (typeof o.facebook.appId !== 'undefined') {
         metas.push({
             name: 'fb:app_id',
-            content: getTwitterUserName(o.facebook.appId)
-        })
+            content: getTwitterUserName(o.facebook.appId),
+        });
     }
 
-    if (typeof o.returnArray !== 'undefined')
-        returnArray = o.returnArray
+    if (typeof o.returnArray !== 'undefined') returnArray = o.returnArray;
 
-    if (returnArray) return metas
-    return metas.map(meta => {
-        let html = '<meta'
-        for (let i in meta) {
-            html += ` ${i}="${meta[i]}"`
-        }
-        html += '/>'
-        return html
-    }).join('')
-}
+    if (returnArray) return metas;
+    return metas
+        .map((meta) => {
+            let html = '<meta';
+            for (const i in meta) {
+                html += ` ${i}="${meta[i]}"`;
+            }
+            html += '/>';
+            return html;
+        })
+        .join('');
+};
 
-const getTwitterUserName = input => {
-    if (typeof input !== 'string') input = input + ''
-    if (input.substr(0, 1) !== '@') input = '@' + input
-    return input
-}
+const getTwitterUserName = (input) => {
+    if (typeof input !== 'string') input = input + '';
+    if (input.substr(0, 1) !== '@') input = '@' + input;
+    return input;
+};
--- test/base.test.js
+++ test/base.test.js
@@ -1,64 +1,70 @@
-let metas
-
+let metas;
 
 describe('Base functions/utilities', () => {
-
     describe('Importing...', function () {
         it(`should be success without error`, () => {
-            metas = require('../index.js')
+            metas = require('../index.js');
         });
     });
 
     describe('Get result as arrays...', function () {
         it(`test #1`, function () {
-            expect(metas({}, true)).toEqual(
-                [{
-                    charset: 'utf-8'
-                }]
-            );
+            expect(metas({}, true)).toEqual([
+                {
+                    charset: 'utf-8',
+                },
+            ]);
         });
         it(`test #2`, function () {
-            expect(metas({ returnArray: true })).toEqual(
-                [{
-                    charset: 'utf-8'
-                }]
-            );
+            expect(metas({ returnArray: true })).toEqual([
+                {
+                    charset: 'utf-8',
+                },
+            ]);
         });
         it(`test #1`, function () {
-            expect(metas({
-                title: "TITLE"
-            }, true)).toEqual(
-                [{
-                    charset: 'utf-8'
-                }, {
+            expect(
+                metas(
+                    {
+                        title: 'TITLE',
+                    },
+                    true
+                )
+            ).toEqual([
+                {
+                    charset: 'utf-8',
+                },
+                {
                     itemprop: 'name',
-                    content: 'TITLE'
-                }, {
+                    content: 'TITLE',
+                },
+                {
                     name: 'twitter:title',
-                    content: 'TITLE'
-                }, {
+                    content: 'TITLE',
+                },
+                {
                     property: 'og:title',
-                    content: 'TITLE'
-                }]
-                );
+                    content: 'TITLE',
+                },
+            ]);
         });
     });
 
     describe('Get result as HTML...', function () {
         it(`test #1`, function () {
-            expect(metas({})).toBe(
-                `<meta charset="utf-8"/>`
-            );
+            expect(metas({})).toBe(`<meta charset="utf-8"/>`);
         });
         it(`test #2`, function () {
-            expect(metas({
-                title: "TITLE"
-            })).toBe(
-                '<meta charset="utf-8"/>'
-                + '<meta itemprop="name" content="TITLE"/>'
-                + '<meta name="twitter:title" content="TITLE"/>'
-                + '<meta property="og:title" content="TITLE"/>'
-                );
+            expect(
+                metas({
+                    title: 'TITLE',
+                })
+            ).toBe(
+                '<meta charset="utf-8"/>' +
+                    '<meta itemprop="name" content="TITLE"/>' +
+                    '<meta name="twitter:title" content="TITLE"/>' +
+                    '<meta property="og:title" content="TITLE"/>'
+            );
         });
     });
-})
+});
